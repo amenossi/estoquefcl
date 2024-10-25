5ecd09ac6c04221960746dc8c61e834820c38fcf
amenossi <allan_menossii@hotmail.com>
2024-10-25 08:04:33 -0300

    Add files via upload

---
+++ programMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using EstoqueFacil.Menus;
+using System.Collections.Generic;
+
+namespace EstoqueFacil
+{
+    public class ProgramMain
+    {
+        private Dictionary<int, Menu> opcoes = new Dictionary<int, Menu>();
+        public ProgramMain()
+        {
+            opcoes.Add(1, new menuAdicionar());
+            opcoes.Add(2, new menuAlterar());
+            opcoes.Add(3, new menuListar());
+            opcoes.Add(4, new menuExcluir());
+            opcoes.Add(5, new menuVender());
+            opcoes.Add(6, new Relatorios());
+            opcoes.Add(7, new MenuBuscarIndividual());
+        }
+
+        public void MenuApresentacao()
+        {
+            Console.Clear();
+            Console.WriteLine("Bem vindo ao Estoque Fácil");
+            Console.WriteLine("1 - Para cadastrar um produto");
+            Console.WriteLine("2 - Para alterar os dados de um produto");
+            Console.WriteLine("3 - Para ver os produtos cadastrados");
+            Console.WriteLine("4 - Para excluir um produto");
+            Console.WriteLine("5 - Para vender um produto");
+            Console.WriteLine("6 - Para gerar um relatório");
+            Console.WriteLine("7 - Pesquisar item");
+            var opc = Console.ReadLine();
+            if (opcoes.ContainsKey(int.Parse(opc)))
+            {
+                Menu menuExibido = opcoes[int.Parse(opc)];
+                menuExibido.Executar();
+            }
+        }
+
+        public static void Main()
+        {
+            ProgramMain programa = new ProgramMain();
+            programa.MenuApresentacao();
+            
+        }
+    }
+}
